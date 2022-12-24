de6df0af77e2d8540ebf11bc80e28a63ca6e1295
Diogo Alves <diogodidi2022@gmail.com>
2022-12-24 00:05:13 -0300

    correcting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+const App = () =>
+(
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>Hello world</h1>
+  </div>
+);
 
-function App() {
-  
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  )
-}
 
 export default App
