750ed4f44eb0a92bd7c7c8d62dd063591380e980
Diogo Alves <diogodidi2022@gmail.com>
2022-12-26 23:06:36 -0300

    removing spaces

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}> 
-        <Stats />
+        <Stats/>
         <Business />
         <Billing />
         <CardDeal />
