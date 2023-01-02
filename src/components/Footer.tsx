55590a86abea7be6bedeb87a0c3cc3a82b062dff
Diogo Alves <diogodidi2022@gmail.com>
2023-01-02 17:32:50 -0300

    starting footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,7 @@
+import styles from "../styles";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
+
 const Footer = () => {
     return(
         <>Footer</>
